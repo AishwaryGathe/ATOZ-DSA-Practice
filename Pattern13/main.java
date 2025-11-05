f36d2dd1231be98a7b9d2e7fc47a3b8358df6cf1
Aishwary Gathe <aishwarygathe@gmail.com>
2025-11-05 07:07:37 +0530

    updating pat13

---
+++ Pattern13/main.java
@@ -0,0 +1,25 @@
+package Pattern13;
+
+import java.util.Scanner;
+
+public class main {
+    public static void pat13(int n)
+    {
+        int num =1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num = num+1;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pat13(n);
+        sc.close();
+    }
+    
+}
