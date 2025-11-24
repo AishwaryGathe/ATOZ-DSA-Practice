a35174eeeb817accbfdc60066710f9a84a729992
Aishwary Gathe <aishwarygathe@gmail.com>
2025-11-24 07:23:49 +0530

    updating pat21

---
+++ Pattern21/Pattern21.java
@@ -0,0 +1,24 @@
+package Pattern21;
+
+import java.util.Scanner;
+
+public class Pattern21 {
+    public static void pat21(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("*");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pat21(n);
+        sc.close();
+    }
+}
