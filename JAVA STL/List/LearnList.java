7e740da929da04822b1ffc3d374fd6a87221cfde
Aishwary Gathe <aishwarygathe@gmail.com>
2025-11-28 08:11:19 +0530

    updating code

--- JAVA STL/List/LearnList.java
+++ JAVA STL/List/LearnList.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class LearnList {
@@ -23,21 +24,46 @@ public class LearnList {
         studentName.add("Shama");
         System.out.print(studentName);
 
-        studentName.add(0,"Veera");
+        studentName.add(0, "Veera");
         System.out.println(studentName);
-
 
         List<String> newList = new ArrayList<>();
 
-        newList.add("Nikita");        
+        newList.add("Nikita");
         newList.add("Ruchita");
         newList.add("Waghmare");
 
         System.out.println(newList);
-        
-        studentName.addAll(newList);
+
+        studentName.addAll(newList); // to merge the list with same datatype
         System.out.println(studentName);
 
+        System.out.println(studentName.get(3)); // to get the element from array
 
+        studentName.remove(4); // to remove the element
+        System.out.println(studentName);
+
+        // studentName.clear(); //to clear all element from the list
+        // System.out.println(studentName);
+
+        studentName.set(1, "Rakesh"); // will add the element in the list.
+        System.out.println(studentName);
+
+        System.out.println(studentName.contains("Nikita"));
+
+        for (int i = 0; i < studentName.size(); i++) {
+            System.out.println("My ClassMates Name Are " + studentName.get(i));
+        }
+
+        for(String element: studentName){
+            System.out.println("For Each Element " + element);
+        }
+
+        Iterator<String> it = studentName.iterator();
+
+        while (it.hasNext()) {
+            System.out.println("Element "+ it.next());
+            
+        }
     }
 }
---
+++ JAVA STL/Stack/stack.java
@@ -0,0 +1,34 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class stack {
+    public static void main(String[] args){
+
+        Stack<String> students = new Stack<>();     
+
+        students.push("Aish");
+        students.push("Swinal");
+        students.push("Rehan");
+        students.push("Harshal");
+        students.push("Rupesh");
+        students.pop();
+
+        // System.out.println(students);
+
+
+        Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(10);
+        pq.add(40);
+        pq.add(10);
+        pq.add(20);
+        pq.add(20);
+        pq.add(30);
+
+        System.out.println(pq);
+
+
+    }
+}
